94723f0571911c7075c1b3b899c207a90998442e
jonathan lopez <jonathandres0309@gmail.com>
2025-04-03 19:35:14 -0500

    jonathan commit2

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 formato = [
-    {"pregunta1":""},
+    {"pregunta1":"C"},
     {"pregunta2":""},
     {"pregunta3":""},
     {"pregunta4":""},
     {"pregunta5":""}
 ]
-print("Bienvenido a preguntas y respuestas","")
+print("Bienvenido a preguntas y respuestas","\n")
+while True: 
+    print("Primera pregunta","\n")
+    print("¿Cual es la capital de Francia?","\n")
+    opcion_a_1 = input("A. Berlin")
+    opcion_a_2 = input("B. Madrid")
+    opcion_a_3 = input("C. Paris")
+    opcion_a_4 = input("D. Roma")
+    
