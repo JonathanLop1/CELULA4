80c3016ef35122ef577f9d27f2cf2cc2d584d667
wilffren <39842857+wilffren@users.noreply.github.com>
2025-04-04 15:45:47 +0000

    version 2.0.3

--- main.py
+++ main.py
@@ -21,6 +21,38 @@ else:
 
 # es un sistema basico pero efectivo para lograr que una opcion sea multiple.
 
+# Pregunta 2
+print("\n2. ¿Cual es el sentido de la vida?")
+print("1. Dios")
+print("2. No lo se")
+print("3. El amor")
+print("4. Los amigos que hicimos en el camino")
+
+respuesta = input("\nTu respuesta (1-4): ")
+if respuesta == "2":
+    print("¡Correcto!")
+    puntaje += 1
+else:
+    print("Incorrecto. La respuesta correcta es 2. No lo se")
+
+# Pregunta 3
+print("\n3. ¿Cuál es el pais con mas poblacion del mundo?")
+print("1. Rusia")
+print("2. Brasil")
+print("3. India")
+print("4. China")
+
+respuesta = input("\nTu respuesta (1-4): ")
+if respuesta == "4":
+    print("¡Correcto!")
+    puntaje += 1
+else:
+    print("Incorrecto. La respuesta correcta es 4. China")
+
+    # hasta aca las preguntas para la seleccion correcta.
+
+# ahora seguira el resumen.
+
 
 
         
