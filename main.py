eb5dc67936206830d7904a32cbb1a7f10f4c234d
wilffren <39842857+wilffren@users.noreply.github.com>
2025-04-04 00:51:21 +0000

    Prueba #1 en rama desarrollo

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 # Formato en forma de diccionario para las preguntas
-formato = [
-    {"pregunta1":""},
-    {"pregunta2":""},
-    {"pregunta3":""},
-    {"pregunta4":""},
-    {"pregunta5":""}
-]
-print("Bienvenido a preguntas y respuestas","")
+# formato = [
+#     {"pregunta1":""},
+#     {"pregunta2":""},
+#     {"pregunta3":""},
+#     {"pregunta4":""},
+#     {"pregunta5":""}
+# ]
+# print("Bienvenido a preguntas y respuestas","")
 
 
 # 
+
+puntaje = 0
+num_preguntas = 3
+
+respuesta = input(f'Serán {num_preguntas} preguntas ¿Preparado para jugar? (si/no) :')
+
+if respuesta.lower() == 'si':
+    respuesta = input('Pregunta 1: ¿Cual es la capital de Hungria?')
+    if respuesta.lower() == 'budapest':
+        puntaje += 1
+        print('¡Correcto! :D')
+    else:
+        print('Incorrecto :(, la respuesta es: Budapest')
+ 
+ 
+    respuesta = input('Pregunta 2: ¿Cual es la capital de Chile?')
+    if respuesta.lower() == 'santiago':
+        puntaje += 1
+        print('¡Correcto! :D')
+    else:
+        print('Incorrecto :(, la respuesta es: Santiago')
+ 
+    respuesta = input('Pregunta 3: ¿Cual es la capital de Ecuador?')
+    if respuesta.lower() == 'quito':
+        puntaje += 1
+        print('correct')
+    else:
+        print('Incorrecto :(, la respuesta es: Quito')
+
+        
+    
